47689a896f19e3a4e405319429fe91d79014690a
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-06-30 19:25:33 +0300

    git commit -m

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const Blog = require('./models/blog')
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
